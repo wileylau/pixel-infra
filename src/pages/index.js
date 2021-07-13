441c4cef8d59ed104d115b8c4ce1c6b332063e63
Wiley Lau <henloboii@protonmail.ch>
2021-07-13 21:55:06 +0800

    pixel-infra: index: fixup description
    Signed-off-by: Wiley Lau <henloboii@protonmail.ch>

--- src/pages/index.js
+++ src/pages/index.js
@@ -30,7 +30,7 @@ export default function Home() {
   return (
     <Layout
       title={`${siteConfig.title}`}
-      description="Description will go into a meta tag in <head />">
+      description="henlo's pixel port kingdom">
       <HomepageHeader />
       <main>
         <HomepageFeatures />
